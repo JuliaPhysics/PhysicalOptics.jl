71f995b36b6726d782c72559f59d6444d03938bc
Documenter.jl <documenter@juliadocs.github.io>
2020-11-05 16:14:18 +0000

    build based on 2e0f4a0

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#PhysicalOptics.jl","page":"PhysicalOptics.jl","title":"PhysicalOptics.jl","text":"","category":"section"},{"location":"","page":"PhysicalOptics.jl","title":"PhysicalOptics.jl","text":"A package for simulation of physical optics. Physical optics is more general than ray optics but not as general as full electrodynamics.","category":"page"},{"location":"","page":"PhysicalOptics.jl","title":"PhysicalOptics.jl","text":"Checkout GitHub for news and source code.","category":"page"}]
+}
