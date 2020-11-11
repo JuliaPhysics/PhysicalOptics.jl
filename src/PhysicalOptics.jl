1ae4d648f6da92e5a3d3c4ed26ef6b37e2a82e88
roflmaostc <fxw+git@mailbox.org>
2020-11-11 19:45:45 +0100

    Incorporate new methods

--- src/PhysicalOptics.jl
+++ src/PhysicalOptics.jl
@@ -3,21 +3,32 @@ module PhysicalOptics
 using FFTW
 using SpecialFunctions
 
- # convolutions
+
+ # some useful functions
+include("utils.jl")
+ # some optics related conversions
+include("physical_conversions.jl")
+ 
+
+ # simple equations to calculate some resolution criterias
+include("resolution_equations.jl")
+
+
+ # fast FFT based convolutions
 include("convolutions.jl")
 
-
-include("utils.jl")
-
- # utils to view conveniently images
+ # utils to get useful objects to view 
 include("utils_view.jl")
 
 
+
+ # light propagation
+include("propagations.jl")
+
+
+ # point spread functions
 include("psf.jl")
 
-
- # simple equations to calculate some resolutions
-include("resolution_equations.jl")
 
 
 end # module
