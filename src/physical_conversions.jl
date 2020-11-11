0c35adccce67978762d12abd840d4d980cfc9796
roflmaostc <fxw+git@mailbox.org>
2020-11-11 19:45:13 +0100

    Add some conversion for \lambda, k and \kappa

---
+++ src/physical_conversions.jl
@@ -0,0 +1,58 @@
+export calc_k, calc_λ, calc_κ
+export calc_NA
+
+"""
+    calc_NA(focal_length, diameter[, n])
+
+Calculate the numerical aperture of a system with `focal_length` 
+and `diameter` of the lens. Per default `n=1` is the refractive index.
+
+ # Examples
+```julia-repl
+julia> calc_NA(100e-3, 200e-3)
+1.0
+
+julia> calc_NA(100e-3, 200e-3, 1.33)
+1.33
+```
+"""
+function calc_NA(focal_length, diameter, n=1)
+    return n * diameter / 2 / focal_length
+end
+
+
+"""
+    calc_k(λ, n=1)
+
+Calculate the value of the (angular) wave number with vacuum wavelength `λ` in
+medium with refractive index `n`.
+It holds: \$ k = \\kappa \\cdot 2 \\pi \$
+"""
+function calc_k(λ, n=1)
+    return 2 * π * n / λ
+end
+
+"""
+    calc_κ(λ, n=1)
+
+Calculate the value of the (non angular) wave number with vacuum wavelength `λ` in
+medium with refractive index `n`.
+It holds: \$ \\kappa = \\frac{k}{2 \\pi} \$
+"""
+function calc_κ(λ, n=1)
+    return calc_k(λ, n) / 2 / π
+end
+
+
+"""
+    calc_λ(k, n=1)
+
+Calculate the vacuum wavelength `λ` from the angular wave number `k` in 
+medium with refractive index `n`.
+It holds: \$ \\lambda = \\frac{n \\cdot 2 \\pi}{k} \$
+""" 
+function calc_λ(k, n=1)
+    return 2 * π * n / k
+end
+
+
