3818bc00cd6c1e5d1b642ce247371243a89c9a76
roflmaostc <fxw+git@mailbox.org>
2020-11-12 23:19:18 +0100

    Incorporate new methods into main

--- src/PhysicalOptics.jl
+++ src/PhysicalOptics.jl
@@ -9,22 +9,24 @@ include("utils.jl")
  # some optics related conversions
 include("physical_conversions.jl")
  
-
  # simple equations to calculate some resolution criterias
 include("resolution_equations.jl")
-
 
  # fast FFT based convolutions
 include("convolutions.jl")
 
+
+ # apertures 
+include("apertures.jl")
+
  # utils to get useful objects to view 
 include("utils_view.jl")
-
-
 
  # light propagation
 include("propagations.jl")
 
+ # some lens functions
+include("lenses.jl")
 
  # point spread functions
 include("psf.jl")
