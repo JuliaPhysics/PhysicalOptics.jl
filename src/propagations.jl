0f3c0aec6ae7d598fe079139cf54e0a28f20f63d
roflmaostc <fxw+git@mailbox.org>
2020-11-12 23:17:11 +0100

    Add a lens propagate function which propagates to back focal plane

--- src/propagations.jl
+++ src/propagations.jl
@@ -1,5 +1,7 @@
 export propagate
 export fresnel_kernel, rs_kernel
+export lens_propagate
+
 
 """
     fresnel_kernel(fx, fy, z, λ, n=1.0)
@@ -52,4 +54,46 @@ function propagate(arr, L, z; kernel=rs_kernel, λ=550e-9, n=1)
     out = ifft(out_ft)
 
     return out
+end
+
+
+"""
+    lens_propagate(arr, L, f; λ=550e-9, n=1, d=nothing)
+
+Propagate an electrical field of width `L` at a distance `d` in
+front of a lens with focal length `f` in medium with refractive index `n`
+to the back focal plane of the lens.
+Per default `d=nothing` meaning that we set `d=f`.
+Assume same sampling and size in both dimensions.
+As return parameter we get the resulting field and the new field size.
+Essentially the lens (assuminig infinite large of that lens) performs
+a scaled Fourier transform. Therefore we get a new field size.
+
+
+ # Reference
+Based on:
+* "Computational Fourier Optics. A MATLAB Tutorial", D. Voelz, (2011).
+* Goodman, Joseph W. Introduction to Fourier optics
+"""
+function lens_propagate(arr, L, f; λ=550e-9, n=1, d=nothing)
+    if isnothing(d)
+        d = f
+    end
+    κ = calc_κ(λ, n)
+    # shift to center again
+    out = fftshift(fft(ifftshift(arr)))
+    dx = L / size(arr)[2]
+    dy = L / size(arr)[1]
+
+    # lens performs scaled fourier transform. Therefore new field size
+    L_new = λ * f / dx
+
+    # complex pre factor of final results
+    for (j, x) in enumerate(fftpos(L_new, size(arr)[2]))
+        for (i, y) in enumerate(fftpos(L_new, size(arr)[1]))
+            c = 1 / (1im * λ * f) * exp(1im * π * κ / f * (1 - d / f) * (x^2 + y^2))
+            out[i, j] *= c * dx * dy
+        end
+    end
+    return out, L_new
 end
