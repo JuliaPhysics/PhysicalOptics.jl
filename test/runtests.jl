e651adb8fae3143e4821288f130a121583baca22
abhro <5664668+abhro@users.noreply.github.com>
2025-05-10 19:27:43 -0400

    Encapsulate all tests in one bigger testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -21,26 +21,28 @@ tests = [
 ]
 
 
-@testset "Point test" begin
-    p = Point(1.0, 2.0, 3.0)
+@testset "PhysicalOptics.jl" begin
+    @testset "Point test" begin
+        p = Point(1.0, 2.0, 3.0)
 
-    @test typeof(p) === Point{Float64}
-    @test p.x == 1.0
-    @test p.y == 2.0
-    @test p.z == 3.0
-    
-
-    p = Point(1, 2, 3)
-    @test typeof(p) === Point{Int64}
-    @test p.x == 1
-    @test p.y == 2
-    @test p.z == 3
-
-end
+        @test typeof(p) === Point{Float64}
+        @test p.x == 1.0
+        @test p.y == 2.0
+        @test p.z == 3.0
 
 
-for t in tests
-    @testset "$(t)" begin
-        include("$t.jl")
+        p = Point(1, 2, 3)
+        @test typeof(p) === Point{Int64}
+        @test p.x == 1
+        @test p.y == 2
+        @test p.z == 3
+
+    end
+
+
+    for t in tests
+        @testset "$(t)" begin
+            include("$t.jl")
+        end
     end
 end
