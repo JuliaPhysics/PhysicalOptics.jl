4c083e9c15fe945b1e975b2cc75f8a3de02b7de3
abhro <5664668+abhro@users.noreply.github.com>
2025-05-10 19:38:05 -0400

    Bump compat in docs/Project.toml

--- docs/make.jl
+++ docs/make.jl
@@ -1,15 +1,19 @@
 using Documenter, PhysicalOptics 
 
-
-
 DocMeta.setdocmeta!(PhysicalOptics, :DocTestSetup, :(using PhysicalOptics); recursive=true)
-makedocs(modules=[PhysicalOptics],
-         sitename="PhysicalOptics.jl",
-         doctest = false,
-         pages = Any[
-                "PhysicalOptics.jl" => "index.md",
-         ]
-        )
-         
 
-deploydocs(repo = "github.com/JuliaPhysics/PhysicalOptics.jl.git", devbranch="main")
+makedocs(;
+    modules=[PhysicalOptics],
+    sitename="PhysicalOptics.jl",
+    doctest = false,
+    pages = [
+        "PhysicalOptics.jl" => "index.md",
+    ]
+)
+
+
+deploydocs(;
+    repo = "github.com/JuliaPhysics/PhysicalOptics.jl.git",
+    devbranch = "main",
+    push_preview = true,
+)
